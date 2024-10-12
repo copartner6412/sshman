08a92d6fbf3ef6f87270dd15ee46dc800b9e1110
copartner6412 <foothold4321@tutamail.com>
2024-10-12 20:49:56 +0330

    add ParseHostCertificateRequest function to v3

--- v3/parse_certificate_request.go
+++ v3/parse_certificate_request.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-func ParseCertificateRequest(certificateRequestBytes []byte) (*UserCertificateRequest, error) {
+func ParseUserCertificateRequest(certificateRequestBytes []byte) (*UserCertificateRequest, error) {
 	cr := new(UserCertificateRequest)
 	_, err := asn1.Unmarshal(certificateRequestBytes, cr)
 	if err != nil {
@@ -17,6 +17,34 @@ func ParseCertificateRequest(certificateRequestBytes []byte) (*UserCertificateRe
 
 	if cr.CertificateRequesterUsername == "" {
 		errs = append(errs, errors.New("empty username"))
+	}
+
+	if cr.CertificateRequesterPublicKey == nil {
+		errs = append(errs, errors.New("nil public key"))
+	}
+
+	if cr.Signature == nil {
+		errs = append(errs, errors.New("not signed"))
+	}
+
+	if len(errs) > 0 {
+		return nil, fmt.Errorf("invalid certificate request: %w", errors.Join(errs...))
+	}
+
+	return cr, nil
+}
+
+func ParseHostCertificateRequest(certificateRequestBytes []byte) (*HostCertificateRequest, error) {
+	cr := new(HostCertificateRequest)
+	_, err := asn1.Unmarshal(certificateRequestBytes, cr)
+	if err != nil {
+		return nil, err
+	}
+
+	var errs []error
+
+	if cr.CertificateRequesterSSHAddress == "" {
+		errs = append(errs, errors.New("empty SSH address"))
 	}
 
 	if cr.CertificateRequesterPublicKey == nil {
